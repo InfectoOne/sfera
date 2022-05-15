0f382949e27faeafcc476ecf15b3b1f0bb4323f3
InfectoOne <28678851+InfectoOne@users.noreply.github.com>
2022-05-15 15:10:43 +0200

    Re-initialized client using Quasar CLI

--- sfera-client/src/App.vue
+++ sfera-client/src/App.vue
@@ -1,23 +1,52 @@
 <template>
-  <h2>Sfera</h2>
-  <div>
-  {{ isConnected ? 'Connected' : 'Not connected'}}
-  </div>
-  <div>
-  <h3>Peers online:</h3>
-  <div v-for="peer in peersOnline" :key="peer.nickname">
-    {{peer.nickname}}
-    <input type="file" @change="onFileinputEvent($event)"/>
-  </div>
-  </div>
-  Broadcast message:
-  <input v-model="message"/>
-  <button @click="broadcastMessage()">Send</button>
-  <div style="font-weight: bold; margin: 20px 0 10px 0">
-    Received Messages:
-  </div>
-  <div v-for="(chatMsg, idx) in chatMessageList" :key="idx">
-    {{chatMsg}}
+  <div class="column justify-center items-center">
+    <div
+      v-if="isConnected"
+      class="text-green row items-center"
+    >
+      <span>Connected to {{ serverIp }}:{{ serverPort }} </span>
+      <q-icon name="mdi-check-circle" />
+    </div>
+    <div class="row q-px-lg" style="width: 85%">
+      <!-- invisible file-input element that can be accessed by clicking on the project image (or the placeholder) -->
+      <input
+        ref="fileInput"
+        class="hidden-file-input"
+        type="file"
+        @change="afterPickFile()"
+      >
+      <div
+        v-for="peer in peersOnline"
+        :key="peer.nickname"
+        class="col-6"
+      >
+        <q-card class="q-pa-sm q-ma-xs bg-primary" @click="pickFileForPeer(peer)">
+          <q-icon
+            name="mdi-monitor"
+            size="20px"
+            class="bg-negative q-pa-sm q-mr-sm"
+            style="border-radius: 100%;"
+          />
+          <span class="text-subtitle1">{{ peer.nickname }}</span>
+        </q-card>
+      </div>
+      <!--
+      Broadcast message:
+      <input v-model="message">
+      <button @click="broadcastMessage()">
+        Send
+      </button>
+      <div style="font-weight: bold; margin: 20px 0 10px 0">
+        Received Messages:
+      </div>
+      <div
+        v-for="(chatMsg, idx) in chatMessageList"
+        :key="idx"
+      >
+        {{ chatMsg }}
+      </div>
+          -->
+    </div>
   </div>
 </template>
 
@@ -47,7 +76,6 @@ const broadcastMessage = () => {
 }
 const chatMessageList: Ref<string[]> = ref([])
 wsConnection.onmessage = (ev: MessageEvent) => {
-  console.log(ev.data)
   if (ev.data instanceof Blob) {
     const link = window.document.createElement("a")
     link.href = window.URL.createObjectURL(ev.data as Blob)
@@ -77,8 +105,28 @@ wsConnection.onmessage = (ev: MessageEvent) => {
   }
 }
 
-const onFileinputEvent = (e: InputEvent) => {
-  const file = (e.target as HTMLInputElement).files?.[0] as Blob
-  wsConnection.send(file)
+const fileInput: Ref<HTMLInputElement | null> = ref(null)
+const selectedPeer: Ref<SferaPeer | null> = ref(null)
+const pickFileForPeer = (peer: SferaPeer) => {
+  selectedPeer.value = peer
+  if (fileInput.value) {
+    fileInput.value.click()
+  }
 }
+const afterPickFile = () => {
+  const file = fileInput.value?.files?.[0]
+  if( file) {
+    wsConnection.send(file)
+  }
+}
+
+
 </script>
+
+<style scoped lang="sass">
+.hidden-file-input
+  display: block
+  visibility: hidden
+  width: 0
+  height: 0
+</style>
