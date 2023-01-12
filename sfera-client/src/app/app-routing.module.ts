78dc5c15b2de21745572a523c04ebdb2a55070b4
InfectoOne <28678851+InfectoOne@users.noreply.github.com>
2023-01-12 17:32:10 +0100

    Added snackbar notifications and error handling Linting fixes

--- sfera-client/src/app/app-routing.module.ts
+++ sfera-client/src/app/app-routing.module.ts
@@ -1,18 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { ConnectionPageComponent } from './connection-page/connection-page.component';
-import { PeersPageComponent } from './peers-page/peers-page.component';
-import { SignalingGuard } from './signaling/signaling.guard';
-import { SignalingService } from './signaling/signaling.service';
+import { NgModule } from "@angular/core"
+import { RouterModule, Routes } from "@angular/router"
+import { ConnectionPageComponent } from "./connection-page/connection-page.component"
+import { PeersPageComponent } from "./peers-page/peers-page.component"
+import { SignalingGuard } from "./signaling/signaling.guard"
+import { SignalingService } from "./signaling/signaling.service"
 
 const routes: Routes = [
-  {path: "", title: "Connect", component: ConnectionPageComponent, canActivate : [SignalingGuard] },
-  {path: "peers", title: "Peers", component: PeersPageComponent, canActivate : [SignalingGuard] }
-];
+	{path: "", title: "Sfera | Connect", component: ConnectionPageComponent, canActivate : [SignalingGuard] },
+	{path: "peers", title: "Sfera | Peers", component: PeersPageComponent, canActivate : [SignalingGuard] }
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-  providers: [SignalingService, SignalingGuard]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
+	providers: [SignalingService, SignalingGuard]
 })
 export class AppRoutingModule { }
--- sfera-client/src/app/peer-chip/peer-chip.component.spec.ts
+++ sfera-client/src/app/peer-chip/peer-chip.component.spec.ts
@@ -1,23 +1,23 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing"
 
-import { PeerChipComponent } from './peer-chip.component';
+import { PeerChipComponent } from "./peer-chip.component"
 
-describe('PeerChipComponent', () => {
-  let component: PeerChipComponent;
-  let fixture: ComponentFixture<PeerChipComponent>;
+describe("PeerChipComponent", () => {
+	let component: PeerChipComponent
+	let fixture: ComponentFixture<PeerChipComponent>
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ PeerChipComponent ]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [ PeerChipComponent ]
+		})
+			.compileComponents()
 
-    fixture = TestBed.createComponent(PeerChipComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(PeerChipComponent)
+		component = fixture.componentInstance
+		fixture.detectChanges()
+	})
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+	it("should create", () => {
+		expect(component).toBeTruthy()
+	})
+})
